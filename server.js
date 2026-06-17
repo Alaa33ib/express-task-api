c1d09d7e933164aed59f6d0ae7aca8bd752907c2
Alaa33ib <alaa33ib@gmail.com>
2026-06-17 12:38:12 +0300

    added post, put, and delete.

--- server.js
+++ server.js
@@ -17,6 +17,73 @@ app.get('/tasks', async (req, res, next)=>{
     }
 });
 
+app.post('/tasks', async (req, res, next)=>{
+    try{
+        const data = await fs.readFile("./tasks.json", 'utf-8');
+        const tasks = JSON.parse(data);
+        let newTask = {
+            id: tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1,
+            ...req.body
+        };
+        tasks.push(newTask);
+        await fs.writeFile('./tasks.json', JSON.stringify(tasks, null, 2));
+        res.status(201).json({ success: true, data: newTask});
+    } catch(error){
+        next(error);
+    }
+})
+
+app.put('/tasks/:id', async (req, res, next)=>{
+    try{
+        const data = await fs.readFile("./tasks.json", 'utf-8');
+        const tasks = JSON.parse(data);
+
+        const taskID = Number(req.params.id);
+        const taskIndx = tasks.findIndex(task=>task.id === taskID);
+    
+        if(taskIndx===-1){
+            const error = new Error('Task not found');
+            error.statusCode= 404;
+            return next(error);
+        }
+
+        tasks[taskIndx] = {
+            ...tasks[taskIndx],
+            ...req.body
+        };
+
+        await fs.writeFile('./tasks.json', JSON.stringify(tasks, null, 2));
+        res.status(200).json({ success: true, data: tasks[taskIndx]});
+
+    } catch(error){
+        next(error);
+    }
+})
+
+app.delete('/tasks/:id', async (req, res, next)=>{
+    try{
+        const data = await fs.readFile("./tasks.json", 'utf-8');
+        const tasks = JSON.parse(data);
+
+        const taskID = Number(req.params.id);
+        const taskIndx = tasks.findIndex(task=>task.id === taskID);
+    
+        if(taskIndx===-1){
+            const error = new Error('Task not found');
+            error.statusCode= 404;
+            return next(error);
+        }
+
+        const deletedTask = tasks[taskIndx];
+        tasks.splice(taskIndx, 1);
+
+        await fs.writeFile('./tasks.json', JSON.stringify(tasks, null, 2));
+        res.status(200).json({ success: true, data: deletedTask});
+    } catch(error){
+        next(error);
+    }
+})
+
 app.use((err, req, res, next)=>{
     console.error("Server Error: ", err.stack);
     res.status(err.statusCode || 500).json({
