e60b403548eab20b4acee93c47ed4062c5103fa4
Alaa33ib <alaa33ib@gmail.com>
2026-06-17 14:12:40 +0300

    added a validation middleware file

---
+++ middleware/validation.js
@@ -0,0 +1,47 @@
+import fs from "fs/promises";
+
+export const validateTask = async (req, res, next)=>{
+    try{
+        const {title, priority, progress} = req.body ;
+
+        if (!title || typeof title != 'string' || title.trim()===''){
+            const error  = new Error("Title is required and must be a valid non-empty text");
+            error.statusCode = 400;
+            return next(error);
+        }
+
+        const data = await fs.readFile("./tasks.json", 'utf-8');
+        const tasks = JSON.parse(data);
+
+        if (tasks.some(task=>task.title.toLowerCase().trim() === title.toLowerCase().trim())){
+            const error  = new Error("This task already exists, try a different title");
+            error.statusCode = 400;
+            return next(error);
+        }
+
+        const validPriorities = ['low', 'medium', 'high'];
+        if (!priority || typeof priority !== 'string' || !validPriorities.includes(priority.toLowerCase().trim())){
+            const error  = new Error("Priority is required and should be low, medium, or high.");
+            error.statusCode = 400;
+            return next(error);
+        }
+
+        if(progress === undefined || progress === null){
+            req.body.progress = 0;
+        }
+        else{
+            if (progress > 100 || progress < 0 || !Number.isInteger(progress) ){
+                const error  = new Error("Progress should be a whole number between 0 and 100.");
+                error.statusCode = 400;
+                return next(error);
+
+            }
+                
+        }
+        
+        next();
+
+    } catch(error){
+        next(error);
+    }
+}
